13f2411dfe79c0327fdbea91eb9a509259f2b014
Silver Hein <heinsilver@gmail.com>
2016-09-25 23:48:52 +0300

    Update MainActivity.java

--- java/com/example/silver/basketcalculator/MainActivity.java
+++ java/com/example/silver/basketcalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.silver.basketcalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,15 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
     double num1, num2, sub;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         textSubtractee = (TextView) findViewById(R.id.textSubtractee);
+
         textSubtractor = (TextView) findViewById(R.id.textSubtractor);
+        textSubtractor.setFilters(new InputFilter[]{new InputFilter.LengthFilter(7)});
+
         textResult = (TextView) findViewById(R.id.textResult);
+        textResult.setFilters(new InputFilter[]{new InputFilter.LengthFilter(8)});
+
         btnDone = (Button) findViewById(R.id.btnDone);
+
         textSubtractor.setText(display);
         textResult.setText(result);
 
@@ -36,9 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
             public void onClick(View v) {
 
-                if(display.equals("")) return;
+                if (display.equals("")){
+                    return;
+                }
 
-                if (display.endsWith(".")){
+                if (display.endsWith(".")) {
                     return;
                 }
 
@@ -57,13 +67,14 @@ public class MainActivity extends AppCompatActivity {
             updateScreen();
         }
 
-        if(v.getId() == R.id.btnDot && (display.equals("") || display.contains("."))) {
-           return;
+        if (v.getId() == R.id.btnDot && (display.equals("") || display.contains("."))) {
+            return;
         }
 
-        if((v.getId() == R.id.btn0 || v.getId() == R.id.btn1 || v.getId() == R.id.btn2 ||
+        if ((v.getId() == R.id.btn0 ||
+                v.getId() == R.id.btn1 || v.getId() == R.id.btn2 ||
                 v.getId() == R.id.btn3 || v.getId() == R.id.btn4 ||
-                v.getId() == R.id.btn5 ||  v.getId() ==R.id.btn6 ||
+                v.getId() == R.id.btn5 || v.getId() == R.id.btn6 ||
                 v.getId() == R.id.btn7 || v.getId() == R.id.btn8 ||
                 v.getId() == R.id.btn9) && (display.equals("0"))) {
             clear();
